f4e7da45a768397d471b6aeeaefe4cb1e485a373
HoangNam <lenam7546@gmail.com>
2025-03-22 14:13:04 +0700

    model

---
+++ DataAccess/Models/ApiKey.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Models;
+
+public partial class ApiKey
+{
+    public int ApiKeyId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string ApiKey1 { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
